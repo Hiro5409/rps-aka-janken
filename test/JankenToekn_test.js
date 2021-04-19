8b2742ea008527d1860eca990c712d6f05595142
Hiro5409 <delamtt28@gmail.com>
2021-04-19 11:03:01 +0900

    test: Add burning test

--- test/JankenToekn_test.js
+++ test/JankenToekn_test.js
@@ -34,4 +34,33 @@ contract("JankenToken", accounts => {
       }
     });
   });
+
+  describe("burning", () => {
+    const mintAmount = 100;
+    const burnAmount = 10;
+
+    beforeEach(async () => {
+      await jankenToken.mint(host, mintAmount,{ from: master });
+    });
+
+    it("burn when called by master", async () => {
+      const currentBalance = (await jankenToken.balanceOf(host)).toNumber();
+      await jankenToken.burn(host, burnAmount,{ from: master });
+
+      const actual = (await jankenToken.balanceOf(host)).toNumber();
+      const expected = currentBalance - burnAmount;
+      assert.equal(actual, expected, "balance should decrement by amount");
+    });
+
+    it("throws an error when called from a non-master", async () => {
+      try {
+        await jankenToken.burn(host, burnAmount,{ from: host });
+        assert.fail("burning was not restricted to master");
+      } catch (e) {
+        const expected = "Caller is not a burner";
+        const actual = e.reason;
+        assert.equal(actual, expected, "should not be permitted");
+      }
+    });
+  });
 });
