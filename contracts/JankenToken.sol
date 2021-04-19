25c6eba70ec3685b6ec547cd9b0508e39d041cfa
Hiro5409 <delamtt28@gmail.com>
2021-04-19 11:02:14 +0900

    feat: Create JankenToken contract

---
+++ contracts/JankenToken.sol
@@ -0,0 +1,26 @@
+// SPDX-License-Identifier: UNLICENSED
+pragma solidity 0.6.8;
+
+import "openzeppelin-solidity/contracts/token/ERC20/ERC20.sol";
+import "openzeppelin-solidity/contracts/access/AccessControl.sol";
+import "./interfaces/IJankenToken.sol";
+
+contract JankenToken is ERC20, AccessControl, IJankenToken {
+    bytes32 public constant MINTER_ROLE = keccak256("MINTER_ROLE");
+    bytes32 public constant BURNER_ROLE = keccak256("BURNER_ROLE");
+
+    constructor() public ERC20("JankenToken", "JKT") {
+        _setupRole(MINTER_ROLE, msg.sender);
+        _setupRole(BURNER_ROLE, msg.sender);
+    }
+
+    function mint(address _account, uint256 _amount) external override {
+        require(hasRole(MINTER_ROLE, msg.sender), "Caller is not a minter");
+        _mint(_account, _amount);
+    }
+
+    function burn(address _account, uint256 _amount) public override {
+        require(hasRole(BURNER_ROLE, msg.sender), "Caller is not a burner");
+        _burn(_account, _amount);
+    }
+}
