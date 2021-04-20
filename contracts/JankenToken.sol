3f56004f9686aa0a06fc91643b3c58ba7611cf2d
Hiro5409 <delamtt28@gmail.com>
2021-04-20 15:21:40 +0900

    fix: Delete IJankenToken.sol

--- contracts/JankenToken.sol
+++ contracts/JankenToken.sol
@@ -1,11 +1,11 @@
 // SPDX-License-Identifier: UNLICENSED
+
 pragma solidity 0.6.8;
 
 import "openzeppelin-solidity/contracts/token/ERC20/ERC20.sol";
 import "openzeppelin-solidity/contracts/access/AccessControl.sol";
-import "./interfaces/IJankenToken.sol";
 
-contract JankenToken is ERC20, AccessControl, IJankenToken {
+contract JankenToken is ERC20, AccessControl {
     bytes32 public constant MINTER_ROLE = keccak256("MINTER_ROLE");
     bytes32 public constant BURNER_ROLE = keccak256("BURNER_ROLE");
 
@@ -14,12 +14,12 @@ contract JankenToken is ERC20, AccessControl, IJankenToken {
         _setupRole(BURNER_ROLE, msg.sender);
     }
 
-    function mint(address _account, uint256 _amount) external override {
+    function mint(address _account, uint256 _amount) external {
         require(hasRole(MINTER_ROLE, msg.sender), "Caller is not a minter");
         _mint(_account, _amount);
     }
 
-    function burn(address _account, uint256 _amount) public override {
+    function burn(address _account, uint256 _amount) public {
         require(hasRole(BURNER_ROLE, msg.sender), "Caller is not a burner");
         _burn(_account, _amount);
     }
