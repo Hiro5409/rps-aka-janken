2fa1b79eefba67ed540a480519575038b4b18852
Hiro5409 <delamtt28@gmail.com>
2021-04-27 12:26:11 +0900

    test: Add case when create game before deposit sufficient tokens

--- test/GameFactory_test.js
+++ test/GameFactory_test.js
@@ -1,50 +1,51 @@
+const JankenTokenContract = artifacts.require("JankenToken");
+const GameBankContract = artifacts.require("GameBank");
 const GameFactoryContract = artifacts.require("GameFactory");
 const GameContract = artifacts.require("Game");
 
-contract("GameFactory: deployed", () => {
-  it("it has been deployed", async () => {
-    const factory = await GameFactoryContract.deployed();
-    assert(factory, "game factory was not deployed");;
-  });
-});
+const Hand = {
+  Rock: 0,
+  Paper: 1,
+  Scissors: 2,
+};
 
-contract("GameFactory: createGame", accounts => {
+contract("GameFactory", accounts => {
+  const master = accounts[0];
   const host = accounts[1];
-  const Hand = {
-    Rock: 0,
-    Paper: 1,
-    Scissors: 2,
-  };
-  const salt = web3.utils.toHex('Thank you.');
   const hostHand = Hand.Rock;
+  const salt = web3.utils.toHex('Thank you.');
+  const hostHandHashed = web3.utils.soliditySha3(
+    {
+      type: 'uint8',
+      value: hostHand,
+    },
+    {
+      type: 'bytes32',
+      value: salt,
+    }
+  );
+
   let factory;
-  let game;
-  let tx;
+  let jankenToken;
+  let gameBank;
+  const mintAmount = 100;
+  const betAmount = 5;
 
   beforeEach(async ()=>{
-    factory = await GameFactoryContract.deployed();
-    const hostHandHashed = web3.utils.soliditySha3(
-      {
-        type: 'uint8',
-        value: hostHand,
-      },
-      {
-        type: 'bytes32',
-        value: salt,
+    jankenToken = await JankenTokenContract.new();
+    gameBank = await GameBankContract.new(jankenToken.address);
+    factory = await GameFactoryContract.new();
+  });
+
+  describe("insufficient deposit funds", () => {
+    it("throws an error by insufficient deposit funds", async () => {
+      try {
+        await factory.createGame(gameBank.address, betAmount, hostHandHashed, { from: host });
+      } catch (e) {
+        const expected = "Insufficient token deposited in GameBank";
+        const actual = e.reason;
+        assert.equal(actual, expected, "should not be permitted");
       }
-    );
-    tx = await factory.createGame(hostHandHashed, { from: host });
-    const games = await factory.games();
-    game = await GameContract.at(games[0]);
-  });
-
-  it("create new game", async () => {
-    assert(game, "instance of Game should be present");;
-  });
-
-  it("emits the GameCreated event", async () => {
-    const actual = tx.logs[0].event;
-    const expected = "GameCreated";
-    assert.equal(actual, expected, "events should match");
+    });
   });
 });
