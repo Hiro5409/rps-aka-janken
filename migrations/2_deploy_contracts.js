ded2892cd709f1c186d8b4b6e5d7f0ed1bca4f8f
Hiro5409 <delamtt28@gmail.com>
2021-05-07 16:09:19 +0900

    feat: Deploy and mint tokens by master

---
+++ migrations/2_deploy_contracts.js
@@ -0,0 +1,14 @@
+const JankenToken = artifacts.require("JankenToken");
+
+const initialTokenAmount = 100;
+
+module.exports = async (deployer, _network, accounts) => {
+  const master = accounts[0];
+  const host = accounts[1];
+  const guest = accounts[2];
+
+  await deployer.deploy(JankenToken);
+  const jkt = await JankenToken.deployed();
+  await jkt.mint(host, initialTokenAmount, { from: master });
+  await jkt.mint(guest, initialTokenAmount, { from: master });
+};
