f68d8e03826b481ed31316cf5b7b123daecfa42f
Hiro5409 <delamtt28@gmail.com>
2021-05-10 17:51:21 +0900

    feat: Initialize jkt token

--- contracts/GameBank.sol
+++ contracts/GameBank.sol
@@ -2,5 +2,10 @@
 
 pragma solidity 0.6.8;
 
+import "openzeppelin-solidity/contracts/token/ERC20/IERC20.sol";
 contract GameBank {
+    IERC20 private _token;
+    constructor(address token) public {
+        _token = IERC20(token);
+    }
 }
