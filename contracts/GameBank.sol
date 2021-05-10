130b6ff698c6218902769544cb1ea1f1c85a7a63
Hiro5409 <delamtt28@gmail.com>
2021-05-10 17:54:27 +0900

    feat: Create user and token balance mapping using SafeMath

--- contracts/GameBank.sol
+++ contracts/GameBank.sol
@@ -3,9 +3,17 @@
 pragma solidity 0.6.8;
 
 import "openzeppelin-solidity/contracts/token/ERC20/IERC20.sol";
+import "openzeppelin-solidity/contracts/math/SafeMath.sol";
+
 contract GameBank {
+    using SafeMath for uint256;
     IERC20 private _token;
+    mapping(address => uint256) public _userToBalance;
     constructor(address token) public {
         _token = IERC20(token);
+    }
+
+    function depositToken(uint256 amount) public {
+        _userToBalance[msg.sender] = _userToBalance[msg.sender].add(amount);
     }
 }
