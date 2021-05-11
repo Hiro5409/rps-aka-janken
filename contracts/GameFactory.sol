a40ec3962c212dbb3394ed52c27a61b83592e8b3
Hiro5409 <delamtt28@gmail.com>
2021-05-11 14:59:34 +0900

    feat: Create modifier to require min bet amount

--- contracts/GameFactory.sol
+++ contracts/GameFactory.sol
@@ -7,8 +7,19 @@ import "./IGameBank.sol";
 
 contract GameFactory {
     IGameBank private _gameBank;
+    uint256 public constant _minBetAmount = 5;
 
     constructor(address gameBankAddress) public {
         _gameBank = IGameBank(gameBankAddress);
     }
+
+    modifier isSufficientMinimumBetAmount(uint256 betAmount) {
+        require(betAmount >= _minBetAmount, "requried minimum bet amount");
+        _;
+    }
+
+    function createGame(uint256 betAmount, bytes32 hostHandHashed)
+        public
+        isSufficientMinimumBetAmount(betAmount)
+    {}
 }
