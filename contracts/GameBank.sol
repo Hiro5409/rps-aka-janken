99b4f2e2e66eddc28723f8d4f6bf9bff72702812
Hiro5409 <delamtt28@gmail.com>
2021-05-13 16:44:14 +0900

    test: Add test to withdraw tokens by winner

--- contracts/GameBank.sol
+++ contracts/GameBank.sol
@@ -4,6 +4,7 @@ pragma solidity 0.6.8;
 
 import "openzeppelin-solidity/contracts/token/ERC20/IERC20.sol";
 import "openzeppelin-solidity/contracts/math/SafeMath.sol";
+import "./IGameFactory.sol";
 
 contract GameBank {
     using SafeMath for uint256;
@@ -33,5 +34,33 @@ contract GameBank {
         returns (bool)
     {
         return _gameToUserBalance[msg.sender][user] >= amount;
+    }
+
+    function getGameRewards(address factory, uint256 gameId) external {
+        IGameFactory gameFactory = IGameFactory(factory);
+        require(
+            gameFactory.isGameDecided(gameId),
+            "status is invalid, required Decided"
+        );
+        require(gameFactory.isGameWinner(gameId, msg.sender), "you are loser");
+
+        address winner = msg.sender;
+        (address loser, uint256 amount) = gameFactory.getResult(gameId);
+
+        require(
+            _gameToUserBalance[factory][winner] >= amount,
+            "winner should deposit stakes in advance"
+        );
+        require(
+            _gameToUserBalance[factory][loser] >= amount,
+            "loser should deposit stakes in advance"
+        );
+        _gameToUserBalance[factory][winner] = _gameToUserBalance[factory][
+            winner
+        ]
+            .sub(amount);
+        _gameToUserBalance[factory][loser] = _gameToUserBalance[factory][loser]
+            .sub(amount);
+        require(_token.transfer(winner, amount * 2), "fail to transfer");
     }
 }
--- test/GameBank_test.js
+++ test/GameBank_test.js
@@ -70,4 +70,92 @@ contract("GameBank", accounts => {
       assert.equal(actual, expected, "events should match");
     });
   });
+
+  describe("withdraw", () => {
+    let gameId;
+    const guest = accounts[2];
+    const hostHand = HAND.Rock;
+    const hostHandHashed = getHashedHand(hostHand, SALT);
+    const guestHand = HAND.Paper;
+
+    beforeEach(async () => {
+      await setupGame({ factory, jankenToken, gameBank, master, user: host });
+      await setupGame({ factory, jankenToken, gameBank, master, user: guest });
+      gameId = await createGame({ factory, hostHandHashed, host });
+      await factory.joinGame(gameId, guestHand, { from: guest });
+    });
+
+    it("throws an error when try to get rewards but game status was invalid", async () => {
+      try {
+        await gameBank.getGameRewards(factory.address, gameId, { from: guest });
+        assert.fail("invalid status");
+      } catch (e) {
+        const expected = "status is invalid, required Decided";
+        const actual = e.reason;
+        assert.equal(actual, expected, "should not be permitted");
+      }
+    });
+
+    it("throws an error when try to refund but game status was invalid", async () => {
+      try {
+        await gameBank.getRefundedStake(factory.address, gameId, { from: guest });
+        assert.fail("invalid status");
+      } catch (e) {
+        const expected = "status is invalid, required Tied";
+        const actual = e.reason;
+        assert.equal(actual, expected, "should not be permitted");
+      }
+    });
+
+    describe("get rewards for winning game", () => {
+      beforeEach(async () => {
+        await factory.revealHostHand(gameId, hostHand, SALT, { from: host })
+      });
+
+      it("throws an error when loser try to get rewards", async () => {
+        try {
+          await gameBank.getGameRewards(factory.address, gameId, { from: host });
+          assert.fail("you are loser");
+        } catch (e) {
+          const expected = "you are loser";
+          const actual = e.reason;
+          assert.equal(actual, expected, "should not be permitted");
+        }
+      });
+
+      it("emits the WithdrawToken event", async () => {
+        const tx = await gameBank.getGameRewards(factory.address, gameId, { from: guest });
+        const actual = tx.logs[0].event;
+        const expected = "WithdrawToken";
+        assert.equal(actual, expected, "events should match");
+      });
+
+      it("JKT balance of game bank and winner changes correctly", async () => {
+        const rewardsAmount = BET_AMOUNT * 2;
+        const prevJKTBalanceOfGameBank = (await jankenToken.balanceOf(gameBank.address)).toNumber();
+        const prevJKTBalanceOfGuest = (await jankenToken.balanceOf(guest)).toNumber();
+
+        await gameBank.getGameRewards(factory.address, gameId, { from: guest });
+
+        const JKTBalanceOfGameBank = (await jankenToken.balanceOf(gameBank.address)).toNumber();
+        assert.equal(JKTBalanceOfGameBank, prevJKTBalanceOfGameBank - rewardsAmount, "jkt balance of bank should match");
+
+        const JKTBalanceOfGuest = (await jankenToken.balanceOf(guest)).toNumber();
+        assert.equal(JKTBalanceOfGuest, prevJKTBalanceOfGuest + rewardsAmount, "jkt balance of guest should match");
+      });
+
+      it("Deposit of winner and loser changes correctly", async () => {
+        const prevJKTBalanceOfHostDepositedInBank = (await gameBank._gameToUserBalance(factory.address, host)).toNumber();
+        const prevJKTBalanceOfGuestDepositedInBank = (await gameBank._gameToUserBalance(factory.address, guest)).toNumber();
+
+        await gameBank.getGameRewards(factory.address, gameId, { from: guest });
+
+        const JKTBalanceOfHostDepositedInBank = (await gameBank._gameToUserBalance(factory.address, host)).toNumber();
+        assert.equal(JKTBalanceOfHostDepositedInBank, prevJKTBalanceOfHostDepositedInBank - BET_AMOUNT, "host's deposit should match");
+
+        const JKTBalanceOfGuestDepositedInBank = (await gameBank._gameToUserBalance(factory.address, guest)).toNumber();
+        assert.equal(JKTBalanceOfGuestDepositedInBank, prevJKTBalanceOfGuestDepositedInBank - BET_AMOUNT, "guest's deposit balance should match");
+      });
+    });
+  });
 });
